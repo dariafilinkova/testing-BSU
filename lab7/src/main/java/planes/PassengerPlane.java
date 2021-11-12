cb5f6a40fe344f91af12bd74eab4e9a4864c19f7
Daria Filinkova <daria.filinkova@gmail.com>
2021-11-12 21:38:27 +0300

    Clean code

---
+++ lab7/src/main/java/planes/PassengerPlane.java
@@ -0,0 +1,43 @@
+package planes;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class PassengerPlane extends Plane{
+    private int passengersCapacity;
+
+    public PassengerPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, int passengersCapacity) {
+        super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
+        this.passengersCapacity = passengersCapacity;
+    }
+
+    public void setPassengersCapacity(int passengersCapacity) {
+        this.passengersCapacity = passengersCapacity;
+    }
+
+    public int getPassengersCapacity() {
+        return passengersCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", PassengerPlane.class.getSimpleName() + "[", "]")
+                .add("passengersCapacity=" + passengersCapacity)
+                .add("model='" + model + "'")
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PassengerPlane)) return false;
+        if (!super.equals(o)) return false;
+        PassengerPlane plane = (PassengerPlane) o;
+        return passengersCapacity == plane.passengersCapacity;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), passengersCapacity);
+    }
+}
